10df9a2fb46571118511dadeb9142a79ed6b9fd8
Michael Reed <myk27x@yahoo.com>
2015-09-29 18:07:26 -0400

    set default route other than Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :definitions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+root 'definitions#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
