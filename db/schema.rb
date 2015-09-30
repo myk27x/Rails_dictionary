5b92d98d988bb59d58e09447397fe37c74bb1e4f
Michael Reed <myk27x@yahoo.com>
2015-09-29 22:43:59 -0400

    altered scaffolding stock styling and added styling to other views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929193456) do
+ActiveRecord::Schema.define(version: 20150929194926) do
+
+  create_table "definitions", force: :cascade do |t|
+    t.string   "word"
+    t.string   "meaning"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meanings", force: :cascade do |t|
     t.string   "meaning"
