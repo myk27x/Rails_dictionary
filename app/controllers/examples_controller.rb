80ee5d8041f8d39e24d6645d1654a0cc46d8d0f3
Michael Reed <myk27x@yahoo.com>
2015-10-01 00:51:40 -0400

    added controller and view for examples and added as resource to config/routes.rb

---
+++ app/controllers/examples_controller.rb
@@ -0,0 +1,67 @@
+class ExamplesController < ApplicationController
+  before_action :set_definition, only: [:show, :edit, :update, :destroy]
+
+  # GET /examples
+  def index
+  end
+
+  # GET /examples/1
+  def show
+  end
+
+  # GET /examples/new
+  def add
+  end
+
+  # GET /examples/1/edit
+  def edit
+  end
+
+  # POST /examples
+  def create
+    @examples = Example.new(example_params)
+
+    respond_to do |format|
+      if @example.save
+        format.html { redirect_to @example, notice: 'Example was successfully created.' }
+        format.json { render :show, status: :created, location: @example }
+      else
+        format.html { render :new }
+        format.json { render json: @example.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /examples/1
+  def update
+    respond_to do |format|
+      if @example.update(example_params)
+        format.html { redirect_to @example, notice: 'Example was successfully updated.' }
+        format.json { render :show, status: :ok, location: @example }
+      else
+        format.html { render :edit }
+        format.json { render json: @example.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /examples/1
+  def destroy
+    @example.destroy
+    respond_to do |format|
+      format.html { redirect_to examples_url, notice: 'Example was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_example
+      @example = Example.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def example_params
+      params.require(:example).permit(:usage)
+    end
+end
