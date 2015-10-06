f08d27ab14c220de69d78674890ea3d4728fc983
Michael Reed <myk27x@yahoo.com>
2015-10-06 16:32:16 -0400

    altered styling to allot for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :definitions
   resources :examples
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+  resources :sessions
+
 root 'definitions#index'
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 get 'search', to: 'definitions#search'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
