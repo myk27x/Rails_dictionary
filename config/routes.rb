442ef1ec6569faf65b8677e9c61ea7810fce783a
Michael Reed <myk27x@yahoo.com>
2015-10-07 15:16:43 -0400

    removed more sessions data as handled by devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :definitions
 
-  resources :sessions do
-    collection do
-      delete :destroy
-    end
-  end
-
   resources :examples
 
   root 'definitions#index'
