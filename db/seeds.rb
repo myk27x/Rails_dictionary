c8da5058ea6e2303ade927133b7f53446599777d
Michael Reed <myk27x@yahoo.com>
2015-10-07 15:12:14 -0400

    added faker, seeds, and allows homepage and search without login
    fails at login 'No route matches [POST] /sessions/user'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
 User.create(email: 'gavin@example.com', password: 'sekret')
 User.create(email: 'jason@example.com', password: 'alsosekret')
 User.create(email: 'toni@example.com' , password: 'supersekret')
+
+35.times do
+  Definition.create(word: Faker::Lorem.word, meaning: Faker::Lorem.sentence)
+end
